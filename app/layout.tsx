7e33b7853c2894f8faa88dce632fb238fed411c9
shalri <fsaonlinework777@gmail.com>
2024-09-05 01:55:17 +0800

    Intro component desktop layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,9 +18,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${barlow.className} flex min-h-screen flex-col scroll-smooth text-[18px] antialiased`}
+        className={`${barlow.className} max-w-screen flex min-h-screen flex-col overflow-x-hidden scroll-smooth text-[18px] antialiased`}
       >
-        <div className="hi-[64%] absolute right-0 h-[425px] w-[50%] rounded-bl-[60px] bg-pt-light-grayish-blue" />
+        <div className="hi-[64%] absolute right-0 h-[425px] w-[50%] rounded-bl-[60px] bg-pt-light-grayish-blue sm:w-[49%]" />
         {children}
       </body>
     </html>
--- components/Intro.tsx
+++ components/Intro.tsx
@@ -3,10 +3,9 @@ import Image from "next/image";
 
 export default function Intro() {
   return (
-    <main className="overflow-hidden">
-      <div className="relative mb-[86px] mt-[58px] h-[245px] w-[140%]">
-        {/* <div className="relative h-[calc(100vw*0.65)] md:h-[calc(100vw*0.4)] lg:h-[calc(100vw*0.3)]"> */}
-        <div className="relative h-[calc(100vw*0.65)] sm:h-[calc(100vw*0.4)]">
+    <main className="overflow-hidden sm:mx-auto sm:mt-[58px] sm:grid sm:max-w-[1174] sm:grid-cols-2 sm:items-center sm:overflow-visible">
+      <div className="relative mb-[86px] mt-[58px] h-[245px] w-[140%] sm:col-start-2 sm:mb-0 sm:mt-0 sm:h-auto sm:w-[168.5%] sm:pl-8">
+        <div className="relative h-[calc(100vw*0.65)] max-h-[300px] sm:max-h-[575px]">
           <Image
             src="/images/illustration-devices.svg"
             fill
@@ -16,7 +15,7 @@ export default function Intro() {
         </div>
       </div>
 
-      <section className="px-8">
+      <section className="px-8 sm:col-start-1 sm:row-start-1 sm:self-center">
         <div className="flex items-center gap-x-4 font-barlow-condensed uppercase">
           <div className="rounded-full bg-pt-very-dark-blue px-[10px] py-[1px] text-[16px] font-bold tracking-wider text-white">
             {details.status}
@@ -26,19 +25,19 @@ export default function Intro() {
           </div>
         </div>
         <div className="mt-4">
-          <div className="font-barlow-condensed text-[40px] font-bold uppercase leading-[1] text-pt-very-dark-blue">
+          <div className="font-barlow-condensed text-[40px] font-bold uppercase leading-[1] text-pt-very-dark-blue sm:text-[64px]">
             {details.header}
           </div>
-          <div className="mt-4 max-w-[292px] text-pt-dark-grayish-blue">
+          <div className="mt-4 max-w-[280px] text-pt-dark-grayish-blue sm:mt-8">
             {details.description}
           </div>
         </div>
 
-        <div className="mb-[84px] mt-6 flex items-center gap-x-5 uppercase">
-          <div className="rounded-md bg-pt-red px-[25px] py-[10px] font-barlow-condensed text-[14px] font-bold tracking-[0.066em] text-white">
+        <div className="mb-[84px] mt-6 flex items-center gap-x-5 uppercase sm:mb-0 sm:mt-[60px] sm:gap-x-6">
+          <div className="rounded-md bg-pt-red px-[25px] py-[10px] font-barlow-condensed text-[14px] font-bold tracking-[0.066em] text-white sm:flex-shrink-0 sm:px-8 sm:py-[14px] sm:text-[16px]">
             Schedule a demo
           </div>
-          <div className="font-barlow-condensed text-[14px] font-normal tracking-[0.180em] text-pt-grayish-blue">
+          <div className="font-barlow-condensed text-[14px] font-normal tracking-[0.180em] text-pt-grayish-blue sm:flex-shrink-0 sm:tracking-[0.380em]">
             To see a preview
           </div>
         </div>
